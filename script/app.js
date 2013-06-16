2cd2578cf6de5ef85c74757fa328bfd4390c8fec
Maxim Syrykh (slogger) <slogger1994@gmail.com>
2013-06-16 21:09:51 +0600

    APP added

---
+++ script/app.js
@@ -0,0 +1,33 @@
+
+var app = angular
+	.module('uprt', [])
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	  $routeProvider.
+	    when('', {
+	      templateUrl:'views/downloadImage.html', 
+	      controller:'DownloadCtrl'
+	    }).
+	    when('/result', {
+	      templateUrl: 'views/results.html'
+	    }).
+	    when('/process', {
+	      templateUrl: 'views/process.html', 
+	      controller:'cropCtrl'
+	    }).
+	    otherwise({ redirectTo: '/' })
+	    ;
+
+	    $locationProvider.html5Mode(false);
+	  }]);
+
+function MainCtrl ($scope) {
+	$scope.title = "APPLE.UPRT.RU";
+	$scope.footer = "Дезигн ис симпле";
+}
+
+function DownloadCtrl ($scope) {
+	$scope.btnText = "Загрузите фотографию";
+}
+function cropCtrl ($scope) {
+	$scope.btnText = "Далее";
+}
